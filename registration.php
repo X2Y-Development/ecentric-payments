4f0e064d1f041cac9f8f5864ba3ad6c33286141d
Valentyn Varuk <valentyn@x2y.io>
2023-08-28 18:30:23 +0300

    Initial commit, cleanup code.

--- registration.php
+++ registration.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * @author     X2Y Development team <dev@x2y.io>
+ * @copyright  2023 X2Y Development team
+ */
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Ecentric_Payment',
-    __DIR__
-);
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ecentric_Payment', __DIR__);
--- view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-   map: {
-       '*': {
-           ecentricjs: 'Ecentric_Payment/js/ecentric',
-           ecentricspro1js: 'Ecentric_Payment/js/ecentricspro1',
-           ecentricspro2js: 'Ecentric_Payment/js/ecentricspro2',
-       }
-   }
-};
