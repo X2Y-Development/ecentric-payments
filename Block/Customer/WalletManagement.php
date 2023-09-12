94cf47b4f5b5c92d911352809af5a36aefe3bc24
Andrii Cherneha <andrii@x2y.io>
2023-09-12 15:24:25 +0300

    #13 Wallet management.

---
+++ Block/Customer/WalletManagement.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @author     X2Y Development team <dev@x2y.io>
+ * @copyright  2023 X2Y Development team
+ */
+
+declare(strict_types=1);
+
+namespace Ecentric\Payment\Block\Customer;
+
+use Ecentric\Payment\Helper\Data as EcentricHelper;
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class WalletManagement extends Template
+{
+    /**
+     * @param EcentricHelper $ecentricHelper
+     * @param Session $customerSession
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        protected EcentricHelper $ecentricHelper,
+        protected Session        $customerSession,
+        Context                  $context,
+        array                    $data = []
+    )
+    {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWalletManagementUrl(): string
+    {
+        return $this->ecentricHelper->getApiUrl() . '/wallet/manage';
+    }
+
+    /**
+     * @return array
+     */
+    public function getWalletParameters(): array
+    {
+        $merchantId = $this->ecentricHelper->getMerchantId();
+        $merchantKey = $this->ecentricHelper->getMerchantKey();
+        $userId = $this->customerSession->getCustomerId();
+        $checksum = hash('sha256', $merchantKey . '|' . $merchantId . '|' . $userId);
+
+        return [
+            'MerchantID' => $merchantId,
+            'UserID'     => $userId,
+            'Checksum'   => strtoupper($checksum),
+        ];
+    }
+
+}
